4363a237836a6e1e33247938a417ded26bd58518
Guruh Edi Purwanto <guruhedipurwanto@gmail.com>
2019-12-25 11:28:16 +0800

    modify addBookToLibrary function

--- library.js
+++ library.js
@@ -13,11 +13,7 @@ MakeBook.prototype.info = function() {
   return `${this.title} by ${this.author}, ${this.pages} pages, ${this.haveRead}`
 }
 
-function addBookToLibrary() {
-  let title = prompt("Book title", "title")
-  let author = prompt("Book author", "author")
-  let pages = prompt("Book number of pages", "0")
-  let haveRead = prompt("Have you read it?", "yes")
+function addBookToLibrary(title, author, pages, haveRead) {
   let newBook = new Book(title, author, pages, haveRead)
   library.push(newBook)
 }
